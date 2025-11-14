2efbbd99edf8ded4fbe989a6c2507a2fb560d891
rockStar378 <shahzadluqman092@gmail.com>
2025-11-14 18:43:15 +0530

    Uploaded via Bot

---
+++ IstkharMusic/utils/logger.py
@@ -0,0 +1,33 @@
+from pyrogram.enums import ParseMode
+
+from kavyamusic import app
+from kavyamusic.utils.database import is_on_off
+from config import LOGGER_ID
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(2):
+        logger_text = f"""
+<b>{app.mention} 𝗠𝗨𝗦𝗜𝗖 𝗥𝗘𝗖𝗢𝗥𝗗𝗦</b>
+
+<b>𝗖𝗛𝗔𝗧 𝗜𝗗 :</b> <code>{message.chat.id}</code>
+<b>𝗖𝗛𝗔𝗧 𝗡𝗔𝗠𝗘:</b> {message.chat.title}
+<b>𝗖𝗛𝗔𝗧 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 :</b> @{message.chat.username}
+
+<b>𝗨𝗦𝗘𝗥 𝗜𝗗:</b> <code>{message.from_user.id}</code>
+<b>𝗡𝗔𝗠𝗘 :</b> {message.from_user.mention}
+<b>𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘 :</b> @{message.from_user.username}
+
+<b>𝗤𝗨𝗘𝗥𝗬 :</b> {message.text.split(None, 1)[1]}
+<b>𝗦𝗧𝗥𝗘𝗔𝗠𝗧𝗬𝗣𝗘 :</b> {streamtype}"""
+        if message.chat.id != LOGGER_ID:
+            try:
+                await app.send_message(
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return

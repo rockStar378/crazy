3cd7ca5b0f13c2c806a70345a38c7db2a0466bde
rockStar378 <shahzadluqman092@gmail.com>
2025-11-14 18:41:21 +0530

    Uploaded via Bot

---
+++ IstkharMusic/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from kavyamusic import LOGGER, app, userbot
+from kavyamusic.core.call import ankit
+from kavyamusic.misc import sudo
+from kavyamusic.plugins import ALL_MODULES
+from kavyamusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("kavyamusic.plugins" + all_module)
+    LOGGER("kavyamusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await ankit.start()
+    try:
+        await ankit.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("kavyamusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await ankit.decorators()
+    LOGGER("kavyamusic").info(
+      "-1002447776304"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("kavyamusic").info("Stopping ankit Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
